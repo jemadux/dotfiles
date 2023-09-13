fece68889e4c70eeda0d09257ce321f29103570c
Klearchos-Angelos Gkountras <jemadux@cryptolab.net>
2023-09-13 19:03:19 +0300

    changes zsh and some plugins

---
+++ .oh-my-zsh/custom/plugins/paths/paths.plugin.zsh
@@ -0,0 +1,6 @@
+#local bin
+PATH="$HOME/.local/bin:$PATH"
+#latex
+PATH=/usr/local/texlive/2023/bin/x86_64-linux:$PATH
+eMANPATH=$MANPATH:/usr/local/texlive/2023/texmf-dist/doc/man
+INFOPATH=$INFOPATH:/usr/local/texlive/2023/texmf-dist/doc/info
