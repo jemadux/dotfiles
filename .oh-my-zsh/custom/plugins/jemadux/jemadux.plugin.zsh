5e443ce75071ada2079c9a815210e71aef0eb3ea
Klearchos-Angelos Gkountras <jemadux@cryptolab.net>
2023-09-15 18:08:14 +0300

    changing my alias

--- .oh-my-zsh/custom/plugins/jemadux/jemadux.plugin.zsh
+++ .oh-my-zsh/custom/plugins/jemadux/jemadux.plugin.zsh
@@ -30,9 +30,10 @@ alias	ffmpeg='ffmpeg -hide_banner'
 alias	ls='ls -hN --color=auto --group-directories-first'
 alias	grep='grep --color=auto'
 alias	diff='diff --color=auto'
-alias grep='grep --color=auto'
-alias fgrep='fgrep --color=auto'
-alias egrep='egrep --color=auto'
+alias   grep='grep --color=auto'
+alias   fgrep='fgrep --color=auto'
+alias   egrep='egrep --color=auto'
+alias	bat='batcat'
 ### Package Manager Apt
 alias	aptup='sudo apt update && sudo apt upgrade'
 alias	aptupd='sudo apt update'
@@ -53,24 +54,3 @@ alias metefrase='trans -t el '
 alias enose='pdfunite *.pdf out.pdf'
 alias png2pdf='convert *.png out.pdf'
 alias screencast=''mplayer -tv driver=v4l2:width=320:height=240: -vo xv tv:// -geometry '100%:100%' -noborder -ontop
-
-##opensuse zypper
-## UPDATE
-alias zupd='sudo zypper dup && sudo zypper clean && sudo zypper purge-kernels && sudo rm /tmp/* -rf && sudo journalctl --vacuum-time=1d && flatup && flatclear && flatclean'
-alias zup='sudo zypper dup' 
-alias flatup='sudo flatpak update'
-alias flatclean='sudo flatpak uninstall --unused'
-alias flatclear='sudo rm -rf /var/tmp/flatpak-cache*'
-alias pipup='pip install --upgrade pip'
-alias zorphan='sudo zypper pa --orphaned'
-alias zclean='sudo zypper cc'
-alias zver='sudo zypper verify'
-alias libre='free -h && sudo sysctl -w vm.drop_caches=3 && sudo sync && echo 3 | sudo tee /proc/sys/vm/drop_caches && free -h'
-alias zse='zypper se'
-alias zin='sudo zypper in' 
-alias zar='sudo zypper ar -f -n'
-
-##Distrobox cli applications 
-alias ani-cli='distrobox-enter aur -- ani-cli'
-alias setoolkit='distrobox-enter kali -- sudo setoolkit'
-alias wifite='distrobox-enter kali -- sudo wifite' 
