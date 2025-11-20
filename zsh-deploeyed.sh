db47896b534bf433e3b44f58d85e6d31864255c4
Klearchos-Angelos Gkountras <jemadux@cryptolab.net>
2025-11-20 14:51:14 +0200

    spaceship prompt and settings

--- zsh-deploeyed.sh
+++ zsh-deploeyed.sh
@@ -8,5 +8,8 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/p
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git $ZSH_CUSTOM/plugins/zsh-autocomplete
 git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
-
-yes | cp -riv  myzshrc  ~/.zshrc
+##Spaceship promt 
+git clone https://github.com/spaceship-prompt/spaceship-prompt.git "$ZSH_CUSTOM/themes/spaceship-prompt" --depth=1
+ln -s "$ZSH_CUSTOM/themes/spaceship-prompt/spaceship.zsh-theme" "$ZSH_CUSTOM/themes/spaceship.zsh-theme"
+cp $PWD/.config/spaceship.zsh ~/.config/spaceship.zsh
+yes | cp -riv  $PWD/myzshrc  ~/.zshrc
